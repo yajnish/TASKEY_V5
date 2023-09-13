9ef11a10ea96797917283b5424194ea3678c6819
Yajnish <yajnishchacs7499@gmail.com>
2023-09-13 15:53:50 +0400

    13/09 15:54 YC

--- TASKEYV5/Controllers/UserController.cs
+++ TASKEYV5/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TASKEYV5.Data;
+using TASKEYV5.Models.Domains;
+using TASKEYV5.Models.DTO;
 
 namespace TASKEYV5.Controllers
 {
@@ -7,5 +10,35 @@ namespace TASKEYV5.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+        public UserController(ApplicationDbContext dbContext) { 
+            this.dbContext = dbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserRequestDto request)
+        {
+            var user = new User
+            {
+                FirstName = request.FirstName,
+                SecondName = request.SecondName,
+                Position = request.Position,
+                IsManager = request.IsManager
+
+            };
+            await dbContext.User.AddAsync(user);
+            await dbContext.SaveChangesAsync();
+
+            var response = new UserDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                SecondName= user.SecondName,
+                Position = user.Position,
+                IsManager = user.IsManager
+
+            };
+
+            return Ok(user);
+        }
     }
 }
---
+++ TASKEYV5/Models/DTO/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TASKEYV5.Models.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Position { get; set; }
+        public bool IsManager { get; set; }
+    }
+}
